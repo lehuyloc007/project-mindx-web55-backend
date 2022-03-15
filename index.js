822b8e6e94cdce92f7c6cc642cfd479ba38ee442
lehuyloc007 <79748205+lehuyloc007@users.noreply.github.com>
2022-03-15 17:08:16 +0700

    update port

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ app.use(express.json());
 app.use("/", router);
 app.use("/assets", express.static('assets'));
 
+const port = process.env.PORT || 5001
 mongoose.connect('mongodb+srv://admin:TyYwrMr3fkD0a7P7@cluster0.exdre.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(() => {
     console.log('Connected to DB');
-    app.listen(5001, () => {
-        console.log("app is running at 5001")
+    app.listen(port, () => {
+        console.log("app is running at" + port)
     })
 }).catch((err) => {
     console.log('err', err)

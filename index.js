41f84d87d6faeee8c5034bfb89ca0b45d180b995
lehuyloc007 <79748205+lehuyloc007@users.noreply.github.com>
2022-03-15 17:16:34 +0700

    update port

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const router = require("./routers");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(
+    cors({
+        origin: "*"
+    })
+)
 app.use("/", router);
 app.use("/assets", express.static('assets'));
 
-const port = process.env.PORT || 5001
+const port = process.env.PORT || 5001;
 mongoose.connect('mongodb+srv://admin:TyYwrMr3fkD0a7P7@cluster0.exdre.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(() => {
     console.log('Connected to DB');
     app.listen(port, () => {

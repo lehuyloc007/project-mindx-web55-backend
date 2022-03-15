c18551d5549505e0e1721e7738d093b1fe0d0e83
lehuyloc007 <79748205+lehuyloc007@users.noreply.github.com>
2022-03-15 17:35:22 +0700

    update port3

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const router = require("./routers");
+
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 5000;
+const URI = process.env.DATABASE_URL;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,11 +20,11 @@ app.use(
 app.use("/", router);
 app.use("/assets", express.static('assets'));
 
-const port = process.env.PORT || 5000;
-mongoose.connect('mongodb+srv://admin:TyYwrMr3fkD0a7P7@cluster0.exdre.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(() => {
+
+mongoose.connect(URI).then(() => {
     console.log('Connected to DB');
-    app.listen(port, () => {
-        console.log("app is running at" + port)
+    app.listen(PORT, () => {
+        console.log("app is running at" + PORT)
     })
 }).catch((err) => {
     console.log('err', err)

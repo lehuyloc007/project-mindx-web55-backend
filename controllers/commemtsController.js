1beee03a69f783494d51ee8f6ee1eceac2533ee2
lehuyloc007 <79748205+lehuyloc007@users.noreply.github.com>
2022-03-21 20:02:20 +0700

    Update commemtsController.js

--- controllers/commemtsController.js
+++ controllers/commemtsController.js
@@ -8,16 +8,18 @@ const create = async (result) => {
 };
 
 const getListCommentByIdPost = async (result) => {
-    if(!result.postId) {
-        throw new Error("Don't have Id post");
-    }
-    const listCommentByIdPost = await commentsModel
-        .find({postId: result.postId});  //lấy danh sách comment theo id bài viết
-    const aaa = () => { // lấy thông tin user của từng comment
-      return Promise.all(listCommentByIdPost.map(async (item) => {
+  if (!result.postId) {
+    throw new Error("Don't have Id post");
+  }
+  const listCommentByIdPost = await commentsModel.find({
+    postId: result.postId,
+  });
+  const getNameDisplayComment = () => {
+    return Promise.all(
+      listCommentByIdPost.map(async (item) => {
         const infoUser = await usersModel.findOne({
-          _id: item.userId
-        })
+          _id: item.userId,
+        });
         return {
           // _id: item._id,
           // postId: item.postId,
@@ -25,27 +27,31 @@ const getListCommentByIdPost = async (result) => {
           // content: item.content,
           // nameDisplay: infoUser.nameDisplay
           ...item._doc,
-          nameDisplay: infoUser.nameDisplay
-        }
-      })) 
-    } 
+          nameDisplay: infoUser.nameDisplay,
+        };
+      })
+    ).then((val) => val);
+  };
 
-    
-    return aaa().then(val => val);
-}
+  return getNameDisplayComment();
+};
 
 const update = async (info) => {
-  if(!info._id) {
+  if (!info._id) {
     throw new Error("Update comment don't have Id");
   }
-  const updateComment = await commentsModel.findOneAndUpdate({
-    _id: info._id,
-    userId: info.userId,
-    postId: info.postId
-  }, info, {new: true})
-  if(updateComment == null) {
+  const updateComment = await commentsModel.findOneAndUpdate(
+    {
+      _id: info._id,
+      userId: info.userId,
+      postId: info.postId,
+    },
+    info,
+    { new: true }
+  );
+  if (updateComment == null) {
     throw new Error("Update error");
   }
-  return updateComment
-} 
-module.exports = { create, getListCommentByIdPost ,update };
+  return updateComment;
+};
+module.exports = { create, getListCommentByIdPost, update };

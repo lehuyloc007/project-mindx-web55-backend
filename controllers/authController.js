f12950d0b7b5d615629de5b0a036f1dac15ace0e
lehuyloc007 <79748205+lehuyloc007@users.noreply.github.com>
2022-03-24 09:06:25 +0700

    add change pas

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
-const { findUserByEmail } = require("../common/user");
+const { findUserByEmail, findUserById } = require("../common/user");
 const usersModel = require("../models/usersModel");
 const sendEmail = require("../common/sendEmail");
 
@@ -61,8 +61,30 @@ const register = async (result) => {
             _id: insertedUser._id
         }
     }
-
-
+}
+const changePassword = async (result) => {
+    const existedUser = await findUserById(result.userId);
+    if (!existedUser) {
+        throw new Error("User does not exist")
+    }
+    if (!result.password === existedUser.password) {
+        throw new Error("Old password doesn't match")
+    }
+    const { salt, hashedPassword } = encryptPassword(result.passwordNew);
+    result.salt = salt;
+    result.hashedPassword = hashedPassword;
+    const updateUser = await usersModel.findOneAndUpdate({ _id: existedUser._id },
+        {
+            password: passwordNew,
+            salt: result.salt,
+            hashedPassword: result.hashedPassword
+        },
+        { new: true })
+    return  {
+        infoUser: {
+            _id: updateUser._id
+        }
+    }
 }
 
 const verifiEmail = async (info) => {
@@ -96,4 +118,4 @@ const verifyPassword = (password, user) => {
     const hashedPassword = crypto.pbkdf2Sync(password, user.salt, 10000, 64, 'sha512').toString("hex");
     return hashedPassword == user.hashedPassword;
 }
-module.exports = { register, login, verifiEmail }
+module.exports = { register, login, verifiEmail, changePassword }
--- routers/auth.js
+++ routers/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const AuthCtrl = require("../controllers/authController");
+const {authMdw} = require("../middlewares/auth");
 const messageCode = require("../common/messageCode");
 
 router.get("/verifiemail", async (req, res) => {
@@ -26,6 +27,14 @@ router.post("/register", async (req, res) => {
     try {
         const newUser = await AuthCtrl.register(req.body);
         res.json(messageCode(0, newUser));
+    } catch (err) {
+        res.status(409).json(messageCode(1, err.message));
+    }
+});
+router.patch("/changepassword",authMdw, async (req, res) => {
+    try {
+        const changePassUser = await AuthCtrl.changePassword(req.body);
+        res.json(messageCode(0, changePassUser));
     } catch (err) {
         res.status(409).json(messageCode(1, err.message));
     }

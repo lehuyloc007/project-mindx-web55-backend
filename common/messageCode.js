319fb0608c744632c0f23eada2359fd319bf4b2f
lehuyloc007 <79748205+lehuyloc007@users.noreply.github.com>
2022-05-08 22:25:27 +0700

    add timetable

--- common/messageCode.js
+++
@@ -1,8 +0,0 @@
-const messageCode = (type, message) => {
-    return {
-        messageCode: type == 0 ? 0 : 1,
-        message: type == 0 ? 'success' : 'error',
-        data: message 
-    }
-}
-module.exports = messageCode;
